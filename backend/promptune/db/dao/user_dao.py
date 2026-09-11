c139063b6a326584d6c0f46f06976c1fbe40e54a
minty <126943698+mintyZer0@users.noreply.github.com>
2026-09-11 18:07:41 +0800

    Create user_dao.py for selecting, creating and updating users

---
+++ backend/promptune/db/dao/user_dao.py
@@ -0,0 +1,46 @@
+
+from fastapi import Depends
+from sqlalchemy import select, update, insert
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from promptune.db.dependencies import get_db_session
+from promptune.db.models.users import User
+from typing import Optional
+
+class UserDAO:
+    """Class for accessing dummy table."""
+
+    def __init__(self, session: AsyncSession = Depends(get_db_session)) -> None:
+        self.session = session
+
+    async def get_by_username(self, username:str) ->  User | None:
+        """Find a user by their username"""
+        query = select(User).where(User.subsonic_username == username)
+        result = await self.session.execute(query)
+        return result.scalar_one_or_none()
+
+    async def create_user(self, server_url:str, username:str, token:str, salt:str) -> None:
+        new_user = User(
+            subsonic_server_url=server_url,
+            subsonic_username=username,
+            subsonic_token=token,
+            subsonic_salt=salt
+        )
+
+        self.session.add(new_user)
+
+        await self.session.commit()
+
+
+    async def update_user(self, server_url:str, username:str, token:str, salt:str) -> None:
+        statement = (update(User)
+                     .where(User.subsonic_username == username)
+                     .values(
+                        subsonic_server_url=server_url,
+                        subsonic_username=username,
+                        subsonic_token=token,
+                        subsonic_salt=salt
+                        )
+                    )
+        await self.session.execute(statement)
+        await self.session.commit()
